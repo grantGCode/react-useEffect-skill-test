0a3a03160f283eec4f9062c70675a60238a3b627
Grant Bentley <gbentley0618@comcast.net>
2023-10-01 15:37:36 -0500

    have useEffect fetch data from Jason place holder

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [toDoListItems, setListItems] = useState([])
+  const [toDoListItems, setListItems] = useState()
 
   useEffect(() => {
+    const fetchToDos = async () => {
+      const responce = await fetch('https://jsonplaceholder.typicode.com/todos/');
+      const items = await responce.json()
+
+
+      setListItems(items)  
+    }
     fetchToDos()
-  }, [])
-
-  const fetchToDos = async () => {
-    const responce = await fetch('https://jsonplaceholder.typicode.com/todos/');
-    const items = await responce.json();
-    // const parsedItems =items.parse()
-
-    setListItems(...items)
-  }
+  })
+  
 
 
-  return (
+
+  return ( 
     <div >
       <h1>To Do List</h1>
       <div>{JSON.stringify(toDoListItems, null, 2)}</div>
